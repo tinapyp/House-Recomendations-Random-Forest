ac6d2eaebdf337897279785f989feb4370cff585
Fathin Afif <fathinafiff@gmail.com>
2024-05-02 15:43:49 +0700

    add: documentation training

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -20,6 +20,7 @@ def train_model(
     Returns:
         RandomForestClassifier: Trained Random Forest classifier.
     """
+
     # Split into train and test
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
